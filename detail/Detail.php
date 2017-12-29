943b399bebf9acc7d1374932c8a2b3b6ca2c27d9
taylanilkyaz <taylanozgurilkyaz@gmail.com>
2017-12-29 22:00:26 +0300

    details silindi . site css duzenlendi . faq silindi

--- detail/Detail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-class Detail{
-
-
-}
--- faq/gizlilik-politikasi/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require $_SERVER['DOCUMENT_ROOT'].'/system-header.php';
-
-getHeader();
-getNavbar();
-
-require  'gizlilik-politikasi-form.php';
-?>
